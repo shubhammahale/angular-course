f859f9d7e3af0f6c8018987a1af8441cc28c79c4
Shubham <shubhammahale19@gmail.com>
2022-10-12 23:14:51 +0530

    create own components and explore data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,5 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // styles: [
+  //   `
+  //     h3 {
+  //       color: pink;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+}
